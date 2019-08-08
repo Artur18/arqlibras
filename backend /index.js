b502417a6593689f76bcb7db089573ce1ccf0646
Artur18 <arturhur_sant@hotmail.com>
2019-08-08 19:29:36 -0300

    melhoraria no backend

--- backend /index.js
+++ backend /index.js
@@ -1,13 +1,21 @@
-const http = require('http')
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-   if (req.url == 'http://127.0.0.1:3001')
-        res.end('200')
-    res.end(resposta[res.url] || resposta['SemURL'])
-})
-
-server.listen(3001, 'localhost', () => {
-    console.log('Servidor de pé em: http://localhost:3001')
+const server = http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  const filename = "./views/view_palavra" + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': './views/view_palavra'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': './views/navbar'});
+    res.write(data);
+    return res.end();
+  });
+}) 
+server.listen(8080, '', () => {
+    console.log('Servidor de pé em: http://localhost:8080')
     console.log('Pra desligar o servidor: Ctrl + c')
-
-})
+});
